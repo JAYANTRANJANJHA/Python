9dd6544f5a9c83e902c66e46e42ecf9c0f62843d
JAYANT RANJAN JHA <121686579+JAYANTRANJANJHA@users.noreply.github.com>
2026-01-05 13:29:48 +0530

    arthimetic operators

---
+++ Introduction/operators.py
@@ -0,0 +1,33 @@
+"""
+Docstring for Introduction.operators
+
+PEMDAS
+parenthesis()->exponent**->mlt* div/->add+ sub-
+
+
+"""
+#Arithmetic Operator
+
+a=20
+b=10
+
+add=a+b
+print("addition",add)
+
+sub=a-b
+print("subtraction",sub)
+
+mlt=a*b
+print("multiplication",mlt)
+
+div=a/b
+print("division",div)
+
+fdiv=a//b
+print("floordivison",fdiv)
+
+mod=a%b
+print("Modulus",mod)
+
+expo=a**b
+print("exponent",expo)
