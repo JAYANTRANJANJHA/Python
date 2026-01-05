5bbda7e2789ce2ee6ff688f799b0cc9f430dba91
JAYANT RANJAN JHA <121686579+JAYANTRANJANJHA@users.noreply.github.com>
2026-01-05 13:12:27 +0530

    data types

--- Introduction/data_types.py
+++ Introduction/data_types.py
@@ -16,3 +16,56 @@ c=3+4j
 print(a,b,c)
 
 print(type(a),type(b),type(c))
+
+"""
+Boolean-logical operation
+True/False
+
+"""
+is_raining=True
+is_sunny=False
+print(is_raining,is_sunny)
+
+#none type
+result=None
+print(result)
+
+
+#sequence- string,list,tuple
+
+text_str="this is a string , i am a sequence of chars"
+
+print(type(text_str))
+
+
+#list
+
+my_list=['data1','data2','data3']
+print(my_list)
+print(type(my_list))
+
+#tuple
+
+my_tuple=('data1','data2','data3','data4')
+
+#set
+
+unique_numbers={1,2,3,4,5,5,6,6,7}
+print(unique_numbers)
+
+immutable_set=frozenset([1,1,2,3,4,5,8,8])
+print(immutable_set)
+
+
+#mapping data types -dictionary -> key -value pair
+
+person={
+  'name':'gopal','age':100, 'mobile':9889973750083
+}
+
+print(person)
+
+
+
+
+
