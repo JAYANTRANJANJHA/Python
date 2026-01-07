489150455d17f10a7a4439e3ff70cb81e44fd144
JAYANT RANJAN JHA <121686579+JAYANTRANJANJHA@users.noreply.github.com>
2026-01-07 21:39:53 +0530

    Demonstrate Python loops with while, for, range, and loop control statements

--- PyRevCourse/loops.py
+++ PyRevCourse/loops.py
@@ -14,4 +14,34 @@ a=[1,2,3,4,5,6,7,8,9,10]
 for i in a:
   print(i)
 
-b=ra
+#range
+b=list(range(10,0,-1))
+print(b)
+
+#nested loops
+
+for i in range(1,3):
+  for j in range(3,6):
+    print(i,j)
+
+#break statement
+
+for num in range(1,10,1):
+  if num ==7:
+    break
+  print(num) 
+
+#continue
+
+for num in range(1,10,1):
+  if num ==7:
+    continue
+  print(num) 
+
+#pass 
+
+for num in range(1,10,1):
+  if num ==7:
+    pass
+  print(num) 
+          
